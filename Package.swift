d858ffb9aab21b2acee26baa603b5b818fd03337
bowenwp <54548588+bowenwp@users.noreply.github.com>
2021-04-08 23:16:28 -0700

    Update Package.swift for 1.1.4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "Persona",
       url: "https://github.com/persona-id/inquiry-ios/releases/download/1.1.4/Persona.xcframework.zip",
-      checksum: "bd00b1ad7a78416953a516f850fb5cfbc91f0dc2a51ad6ff2a63ea808104aa68"
+      checksum: "88b7ce92fc4957a33ab7ccf836400ee30e522007567575f7d8bcf3dd6e75b380"
     )
   ]
 )
