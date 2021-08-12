bd2f898a43715b822ef8281d7fd88faa84ddfb42
bowenwp <54548588+bowenwp@users.noreply.github.com>
2021-08-12 10:15:53 -0700

    Update Package.swift for 1.1.15

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "Persona",
       url: "https://github.com/persona-id/inquiry-ios/releases/download/1.1.15/Persona.xcframework.zip",
-      checksum: "94869dca0a3a7827e014d4069de3ffbca451530b8d712c4596cb3a8bba32e9a6"
+      checksum: "6e780d118b0d3479f4d4ac791fe962dc7db405b62c91f247ca2bc8f7b69d2f49"
     )
   ]
 )
