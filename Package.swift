d9f8c1f7a5568326190a7231c920622e5ab82fc4
persona-identities <57889169+persona-identities@users.noreply.github.com>
2022-11-10 22:37:58 -0600

    Update Package.swift for 1.1.28

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "Persona",
       url: "https://github.com/persona-id/inquiry-ios/releases/download/1.1.28/Persona.xcframework.zip",
-      checksum: "9938cca59ff509fcd6bb8628f4ade5f5fa4b31e1fc4456b218d80abb9b52f4e7"
+      checksum: "b6b858523c26769e5149956455e7479545c8c8b046e1e8effbf50eb7c50bb1b9"
     )
   ]
 )
